b7f44f5e8298ce7debb28c909d41a1720028a8c0
amirkle27 <amirkle@gmail.com>
2024-07-13 22:22:05 +0300

    Update Homework 11.7.24 no.3 (Bonus Track).py

--- Homework 11.7.24/Homework 11.7.24 no.3 (Bonus Track).py
+++ Homework 11.7.24/Homework 11.7.24 no.3 (Bonus Track).py
@@ -1,2 +1,2 @@
 temp: float = float (input("What's the temperature?"));
-print("freezing" if temp<0 elif "normal"  temp<20 and temp>0 else "hot" if temp>30)
+print ("freezing" if temp<0 else "normal" if temp<20 and temp>=0 else "hot")
